9c69da7d46a7b30c3d884d1bbcd85e2d3fc39001
ungericw <ungericw@tutanota.com>
2020-08-07 18:33:15 -0500

    8/7/2020 step 7 Best practice: use common pointcut configuration

--- src/main/java/com/in28minutes/spring/aop/springaop/aspect/AfterAopAspect.java
+++ src/main/java/com/in28minutes/spring/aop/springaop/aspect/AfterAopAspect.java
@@ -1,6 +1,8 @@
 package com.in28minutes.spring.aop.springaop.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -15,9 +17,18 @@ public class AfterAopAspect {
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	// Weaving and weaver
-	@Before("execution(* com.in28minutes.spring.aop.springaop.business.*.*(..))")
-	public void before(JoinPoint joinPoint) {
-		logger.info("Check for user access ");
-		logger.info(" Intercepted Method Calls - {}", joinPoint);
+	@AfterReturning(
+			value="execution(* com.in28minutes.spring.aop.springaop.aspect.CommonJoinPointConfig.businessLayerExecution()",
+			returning="result"
+			)
+	public void afterReturning(JoinPoint joinPoint, Object result) {
+		logger.info("{} return with value {}", joinPoint, result);
 	}
+	
+	// Weaving and weaver
+	@After(value="execution(* com.in28minutes.spring.aop.springaop.aspect.CommonJoinPointConfig.businessLayerExecution()")
+	public void after(JoinPoint joinPoint) {
+		logger.info("after execution of {} ", joinPoint);
+	}
+	
 }
--- src/main/java/com/in28minutes/spring/aop/springaop/aspect/CommonJoinPointConfig.java
+++ src/main/java/com/in28minutes/spring/aop/springaop/aspect/CommonJoinPointConfig.java
@@ -6,21 +6,19 @@ import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 
-// AOP
-// Configuration
-@Aspect
-@Configuration
-public class UseAccessAspect {
+public class CommonJoinPointConfig {
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
-	// Weaving and weaver
-	@Before("execution(* com.in28minutes.spring.aop.springaop.aspect.CommonJoinPointConfig.dataLayerExecution()")
-	public void before(JoinPoint joinPoint) {
-		logger.info("Check for user access ");
-		logger.info(" Intercepted Method Calls - {}", joinPoint);
+	@Pointcut(value="execution(* com.in28minutes.spring.aop.springaop.data.*.*(..))")
+	public void dataLayerExecution() {
+	}
+	
+	@Pointcut(value="execution(* com.in28minutes.spring.aop.springaop.business.*.*(..))")
+	public void businessLayerExecution() {
 	}
 }
