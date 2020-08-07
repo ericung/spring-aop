ef03a6057ac5c8c7b89423b690733f9b68dc1301
ungericw <ungericw@tutanota.com>
2020-08-06 19:52:01 -0500

    8/6/2020 step 03 defining an @Before advice

--- src/main/java/com/in28minutes/spring/aop/springaop/data/Dao1.java
+++ src/main/java/com/in28minutes/spring/aop/springaop/data/Dao1.java
@@ -1,9 +1,9 @@
-package com.in28minutes.spring.aop.springaop;
+package com.in28minutes.spring.aop.springaop.data;
 
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class Dao2 {
+public class Dao1 {
 	public String retrieveSomething() {
 		return "Dao1";
 	}
